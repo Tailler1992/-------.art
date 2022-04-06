e14d7cbb1cd23c75eee7004c1c709c283719d5b9
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-04-06 21:33:47 +0500

    implemented the display of additional images

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import modals from './modules/modals';
 import sliders from './modules/sliders';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
+import showMoreStyles from './modules/showMoreStyles';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
@@ -10,4 +11,5 @@ window.addEventListener('DOMContentLoaded', () => {
     mask('[name="phone"]');
     checkTextInputs('[name="name"]');
     checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '.styles-2');
 });
