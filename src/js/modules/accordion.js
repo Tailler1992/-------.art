9c0d6ae0a7cc0f04f9b232a404ce7a2a62089cb6
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-04-07 19:32:17 +0500

    burger implemented

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -13,24 +13,6 @@ const accordion = (triggersSelector) => {
             }
         });
     });
-
-
-    // const blocks = document.querySelectorAll(itemsSelector);
-
-    // blocks.forEach(block => {
-    //     block.classList.add('animated', 'fadeInDown');
-    // });
-
-    // btns.forEach(btn => {
-    //     btn.addEventListener('click', function(){
-    //         if (!this.classList.contains('active')) {
-    //             btns.forEach(btn => {
-    //                 btn.classList.remove('active', 'active-style');
-    //             });
-    //             this.classList.add('active', 'active-style');
-    //         }
-    //     })
-    // });
 };
 
 export default accordion;
