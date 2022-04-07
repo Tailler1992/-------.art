75a5419751b774214589415aff9551d5f8f0df49
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-04-07 18:40:46 +0500

    accordion implemented

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
 import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
@@ -18,4 +19,5 @@ window.addEventListener('DOMContentLoaded', () => {
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
     pictureSize('.sizes-block');
+    accordion('.accordion-heading');
 });
