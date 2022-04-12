a8a79cdc004ac4089f57177e7f595e1285ad44a9
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-04-12 22:39:46 +0500

    feat: implemented scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ import filter from './modules/filter';
 import pictureSize from './modules/pictureSize';
 import accordion from './modules/accordion';
 import burger from './modules/burger';
+import scrolling from './modules/scrolling';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
@@ -22,4 +23,5 @@ window.addEventListener('DOMContentLoaded', () => {
     pictureSize('.sizes-block');
     accordion('.accordion-heading');
     burger('.burger-menu', '.burger');
+    scrolling('.pageup');
 });
